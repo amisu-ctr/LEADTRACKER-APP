13236901db8c2a540c06e6c89b372aad504c90ee
amisu-ctr <hamisurahman@gmail.com>
2023-03-19 20:25:17 +0000

    Add chrome tab api to save tab button. when clicked pushes the current tab the user is on into the myLeads array and local storage. Added a few styles and modified html a bit

--- main.js
+++ main.js
@@ -3,12 +3,25 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
 let deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-if(leadsFromLocalStorage){   // even upon refresh set myLeads to local storage to persist rendering on the page
-    myLeads = leadsFromLocalStorage
+if(leadsFromLocalStorage){  
+    myLeads = leadsFromLocalStorage  // even upon refresh set myLeads to local storage to persist rendering on the page
     render(myLeads)
 }
+
+tabBtn.addEventListener("click",function(){
+    //this is a chrome api that grabs hold of the current tabs url 
+    //to make it work i had to set the permissions for it in the manifest.json file
+    //pushed the currenttabs address into myLeads array and render it afterward.
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+} )
 
 //listen for dbclicks on the delete button
 //when clicked , clear localStorage, myLeads and call render() with the emptied myLeads array as its argument
